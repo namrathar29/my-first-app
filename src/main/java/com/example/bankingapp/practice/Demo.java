d4d6f1d23b0547702ab5736ef6965be3ddb6499d
Namratha R <99namratha@gmail.com>
2022-04-26 10:51:49 +0530

    "Added a class and commented few dependencies in the pom."

---
+++ src/main/java/com/example/bankingapp/practice/Demo.java
@@ -0,0 +1,20 @@
+/**
+ *This class is copyright property of DLithe organization. It is open to all and intended to use for with care.
+ */
+package com.example.bankingapp.practice;
+
+/**
+ * This class is used to give live demo on how to commit and push your local changes to your feature branch
+ * @author Namratha R
+ * @version 1.0
+ * @since 1.0
+ * Date: 25/04/2022
+ */
+public class Demo {
+    public void addNumbers(int i, int j){
+        i=10;
+        j=20;
+        System.out.println(i+j);
+    }
+
+}
